294075db4aa8044663a5d0a5019b88174dada6a2
Marek <markjohn666@gmail.com>
2022-03-01 14:11:15 +0100

    Setting up basic routing

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,7 +1,11 @@
 import './menu-item.styles.scss'
+import { withRouter } from 'react-router-dom'
 
-const MenuItem = ({ title, imageUrl, size }) => (
-    <div className={`${size} menu-item`}>
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+    <div
+        className={`${size} menu-item`}
+        onClick={() => history.push(`${match.url}${linkUrl}`)}
+    >
         <div
             className='background-image'
             style={{
@@ -15,4 +19,7 @@ const MenuItem = ({ title, imageUrl, size }) => (
     </div>
 );
 
-export default MenuItem;
+// wrapping with Higher Order Component:
+// it will return super powered MenuItem component
+// with access to location, match and history props
+export default withRouter(MenuItem);
