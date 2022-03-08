fcc9cbf3e868fb31835d13a76e10270dd1b12abb
Marek <markjohn666@gmail.com>
2022-03-08 17:23:59 +0100

    Fix small issue with sign in form

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -68,7 +68,7 @@ class SignIn extends React.Component {
                     {/* type submit will be passed to CustomButton component */}
                     <div className="buttons">
                         <CustomButton type="submit">Sign In</CustomButton>
-                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+                        <CustomButton type="button" onClick={signInWithGoogle} isGoogleSignIn>
                             Sign In with Google
                         </CustomButton>
                     </div>
